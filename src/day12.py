43e3bab92683ce9fc0b688027d0219bc6c2e3dac
Nauhai <noe.mastrorillo@gmail.com>
2022-12-12 22:06:42 +0100

    Optimize day12

--- src/day12.py
+++ src/day12.py
@@ -21,52 +21,48 @@ def parse_grid(input_text):
     return grid, start, end
 
 
-def are_valid_coords(coords, width, height):
+def get_neighbors(coords, grid_width, grid_height):
     x, y = coords
-    return 0 <= x < width and 0 <= y < height
+    for nx, ny in [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]:
+        if 0 <= nx < grid_width and 0 <= ny < grid_height:
+            yield nx, ny
+
+
+def find_path(grid, start, end_pred, reachable_pred):
+    width = len(grid[0])
+    height = len(grid)
+
+    queue = deque([(start, 0)])
+    seen = {start}
+
+    while len(queue) > 0:
+        (x, y), length = queue.popleft()
+
+        if end_pred((x, y)):
+            return length
+        
+        for ncoords in get_neighbors((x, y), width, height):
+            if ncoords not in seen and reachable_pred((x, y), ncoords, grid):
+                queue.append((ncoords, length+1))
+                seen.add(ncoords)
 
 
 def is_reachable(from_coords, to_coords, grid):
     fx, fy = from_coords
     tx, ty = to_coords
     return grid[ty][tx] - grid[fy][fx] <= 1
-
-
-def find_path(grid, start, end):
-    width = len(grid[0])
-    height = len(grid)
-
-    queue = deque([[start]])
-    seen = {start}
-
-    while len(queue) > 0:
-        path = queue.popleft()
-        x, y = path[-1]
-        if (x, y) == end:
-            return path
-        for ncoords in [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]:
-            if are_valid_coords(ncoords, width, height) and ncoords not in seen and is_reachable((x, y), ncoords, grid):
-                queue.append(path + [ncoords])
-                seen.add(ncoords)
     
 
 @solutions.solution(day=12, part=1)
 def part_one(input_text):
-    return len(find_path(*parse_grid(input_text)))-1
+    grid, start, end = parse_grid(input_text)
+    end_pred = lambda c: c == end
+    return find_path(grid, start, end_pred, is_reachable)
 
 
 @solutions.solution(day=12, part=2)
 def part_two(input_text):
     grid, _, end = parse_grid(input_text)
-    width = len(grid[0])
-    height = len(grid)
-
-    min_length = 1000000
-    for y in range(height):
-        for x in range(width):
-            if grid[y][x] == ord('a'):
-                path = find_path(grid, (x, y), end)
-                if path and (length := len(path)-1) < min_length:
-                    min_length = length
-    
-    return min_length
+    end_pred = lambda c: grid[c[1]][c[0]] == ord('a')
+    reachable_pred = lambda f, t, g: is_reachable(t, f, g)
+    return find_path(grid, end, end_pred, reachable_pred)
